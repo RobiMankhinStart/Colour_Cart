6f73d7bd8e708af0b1b7828713206a859386b7d4
Robi Mankhin <mankhinrobi@gmail.com>
2025-11-10 10:50:23 +0600

    making file folder and components

---
+++ app/components/Home/Banner.jsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="bg-[#211c24]">
+      <div className="container">
+        <div className=" py-[188px] pl-[160px] bg-contain bg-no-repeat bg-right bg-[url('/BannnerIphoneImg.png')]">
+          <p className=" text-[#7a777c] text-[25px] font-inter font-semibold">
+            Pro.Beyond.
+          </p>
+          <h1 className="!m-0 my-6 font-thin text-white text-[96px] font-inter">
+            IPhone 14 <span className="font-semibold">Pro</span>
+          </h1>
+          <p className=" mb-6 text-[#909090] text-[18px] font-inter font-medium">
+            Created to change everything for the better. For everyone
+          </p>
+          {/* <Link
+            className="text-white inline-block border-white border rounded-md text-[16px] font-medium font-inter w[191px] py-4 px-[56px]"
+            href={"/products"}
+          >
+            Shop Now
+          </Link> */}
+          <Link
+            href={"/products"}
+            className="relative cursor-pointer py-4 px-8 text-center font-barlow inline-flex justify-center text-base uppercase text-white rounded-lg border-solid transition-transform duration-300 ease-in-out group outline-offset-4 focus:outline focus:outline-2 focus:outline-white focus:outline-offset-4 overflow-hidden"
+          >
+            <span className="relative z-20">Shop Now</span>
+
+            <span className="absolute left-[-75%] top-0 h-full w-[50%] bg-white/20 rotate-12 z-10 blur-lg group-hover:left-[125%] transition-all duration-1000 ease-in-out"></span>
+
+            <span className="w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#D4EDF9] absolute h-[20%] rounded-tl-lg border-l-2 border-t-2 top-0 left-0"></span>
+            <span className="w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#D4EDF9] absolute group-hover:h-[90%] h-[60%] rounded-tr-lg border-r-2 border-t-2 top-0 right-0"></span>
+            <span className="w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#D4EDF9] absolute h-[60%] group-hover:h-[90%] rounded-bl-lg border-l-2 border-b-2 left-0 bottom-0"></span>
+            <span className="w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#D4EDF9] absolute h-[20%] rounded-br-lg border-r-2 border-b-2 right-0 bottom-0"></span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ app/components/common/Navbar.jsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import React from "react";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { IoCartOutline } from "react-icons/io5";
+import { FiUser } from "react-icons/fi";
+import { CiSearch } from "react-icons/ci";
+
+const Navbar = () => {
+  return (
+    <div className="py-4 shadow z-40 bg-[#ffff] fixed top-0 left-0 right-0">
+      <div className="container">
+        <nav className="flex font-semibold justify-between items-center">
+          {/* <Link href={"/"} className="text-[25px] font-inter">
+            OneMegaByte
+          </Link> */}
+          <Link
+            href={"/"}
+            className="relative overflow-hidden bg-gradient-to-r from-green-400 to-blue-500 text-white py-4 px-8 font-semibold rounded-3xl shadow-xl transform transition-all duration-500 hover:scale-110 hover:rotate-3 hover:skew-x-12"
+          >
+            <span className="absolute top-0 left-0 w-full h-full bg-gray-50"></span>
+            <span className="relative z-10 text-gray-600 text-2xl font-inter">
+              <span className="text-3xl text-gray-600">O</span>ne
+              <span className="text-3xl text-gray-600">M</span>ega
+              <span className="text-3xl text-gray-600">B</span>yte
+            </span>
+          </Link>
+          <div className="w-[372px] p-4 bg-[#F5F5F5] text-[#989898] rounded-[8px] flex items-center gap-1">
+            <CiSearch className="text-[24px]" />
+            <input className="outline-none " type="text" placeholder="Search" />
+          </div>
+          <ul className="flex text-[#989898] items-center gap-[52px]">
+            <li className="text-[#989898] hover:text-black font-inter text-[20px] font-medium">
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li className="text-[#989898] hover:text-black font-inter text-[20px] font-medium">
+              <Link href={"/"}>About</Link>
+            </li>
+            <li className="text-[#989898] hover:text-black font-inter text-[20px] font-medium">
+              <Link href={"/"}>Contact Us</Link>
+            </li>
+            <li className="text-[#989898] hover:text-black font-inter text-[20px] font-medium">
+              <Link href={"/register"}>Register</Link>
+            </li>
+          </ul>
+          <div className="flex items-center gap-6">
+            <div className="text-[32px] text-black cursor-pointer hover:text-[#989898] hover:scale-110 duration-200">
+              <IoMdHeartEmpty />
+            </div>
+            <div className="text-[32px] text-black cursor-pointer hover:text-[#989898] hover:scale-110 duration-200">
+              <IoCartOutline />
+            </div>
+            <Link
+              href={"/login"}
+              className="text-[32px] text-black cursor-pointer hover:text-[#989898] hover:scale-110 duration-200"
+            >
+              <FiUser />
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
