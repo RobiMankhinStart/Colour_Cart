518f60aac6ea738d18a369361be23511995f615d
Robi Mankhin <mankhinrobi@gmail.com>
2025-11-12 10:54:33 +0600

    fixing product details page

--- app/components/ProductDetails/ProductDetails.jsx
+++ app/components/ProductDetails/ProductDetails.jsx
@@ -1,20 +1,15 @@
 // "use client";
-// import React, { useRef, useState } from "react";
+// import React, { useState } from "react";
 // // Import Swiper React components
-// import { Swiper, SwiperSlide } from "swiper/react";
 // import Image from "next/image";
 
 // // import Swiper JS
 // // import Swiper from "swiper";
-// import { FreeMode, Navigation, Thumbs, Pagination } from "swiper/modules";
+// import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+// import { Swiper, SwiperSlide } from "swiper/react";
 
 // // import Swiper styles
 // import "swiper/css";
-// import "swiper/css/free-mode";
-// import "swiper/css/navigation";
-// import "swiper/css/thumbs";
-// import "swiper/css/pagination";
-
 // // Import Swiper styles
 // import "swiper/css/free-mode";
 // import "swiper/css/navigation";
@@ -24,16 +19,16 @@
 //   const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
 //   return (
-//     <>
+//     <div className="border">
 //       <Swiper
 //         style={{
 //           "--swiper-navigation-color": "#fff",
 //           "--swiper-pagination-color": "#fff",
 //         }}
-//         loop={true}
+//         // loop={true}
 //         spaceBetween={10}
 //         navigation={true}
-//         thumbs={{ swiper: thumbsSwiper }}
+//         thumbs={thumbsSwiper ? { swiper: thumbsSwiper } : undefined}
 //         modules={[FreeMode, Navigation, Thumbs]}
 //         className="mySwiper2"
 //       >
@@ -49,28 +44,7 @@
 //           </SwiperSlide>
 //         ))}
 //       </Swiper>
-//       {/* <Swiper
-//         onSwiper={setThumbsSwiper}
-//         loop={true}
-//         spaceBetween={10}
-//         slidesPerView={4}
-//         freeMode={true}
-//         watchSlidesProgress={true}
-//         modules={[FreeMode, Navigation, Thumbs]}
-//         className="mySwiper"
-//       >
-//         {product?.images.map((item, index) => (
-//           <SwiperSlide key={index}>
-//             <Image
-//               src={item}
-//               alt="product img"
-//               height={300}
-//               width={300}
-//               className="object-cover rounded-md"
-//             />
-//           </SwiperSlide>
-//         ))}
-//       </Swiper> */}
+
 //       <Swiper
 //         onSwiper={setThumbsSwiper}
 //         loop={true}
@@ -93,76 +67,50 @@
 //           </SwiperSlide>
 //         ))}
 //       </Swiper>
-//     </>
+//     </div>
 //   );
 // }
 "use client";
 
 import React, { useState } from "react";
 import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 
-// Import Swiper styles once only
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/navigation";
-import "swiper/css/thumbs";
-
-export default function ProductDetails({ product }) {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
-
+const ProductDetails = ({ product }) => {
+  const [currantImage, setCurrantImage] = useState(product?.images[0] || "");
   return (
-    <div className="w-full">
-      {/* Main Swiper */}
-      <Swiper
-        style={{
-          "--swiper-navigation-color": "#fff",
-          "--swiper-pagination-color": "#fff",
-        }}
-        loop={true}
-        spaceBetween={10}
-        navigation={true}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper2 rounded-lg"
-      >
-        {product?.images?.map((item, index) => (
-          <SwiperSlide key={index}>
-            <Image
-              src={item}
-              alt="product img"
-              width={500}
-              height={500}
-              className="object-cover rounded-md w-full h-auto"
-            />
-          </SwiperSlide>
+    <div className="flex flex-start gap-4">
+      <div className="flex flex-col gap-2">
+        {product?.images.map((item, index) => (
+          <Image
+            key={index}
+            src={item}
+            alt={`productImage-${index}`}
+            width={150}
+            height={150}
+            className={`cursor-pointer p-2  bg-gray-100 rounded-md w-28 h-28 object-contain opacity-80 hover:opacity-100 duration-200 ${
+              currantImage === item
+                ? "border-2 border-gray-200 opacity-100"
+                : ""
+            }`}
+            onClick={() => setCurrantImage(item)}
+          />
         ))}
-      </Swiper>
-
-      {/* Thumbnail Swiper */}
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        loop={true}
-        spaceBetween={10}
-        slidesPerView={4}
-        freeMode={true}
-        watchSlidesProgress={true}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper mt-4"
-      >
-        {product?.images?.map((item, index) => (
-          <SwiperSlide key={index}>
-            <Image
-              src={item}
-              alt="product thumbnail"
-              width={100}
-              height={100}
-              className="object-cover rounded-md cursor-pointer"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      </div>
+      <div className="bg-gray-100 rounded-md ml-5 w-full max-h-[550px] border-gray-200 shadow-sm flex items-center justify-center cursor-pointer">
+        {product?.images[0] ? (
+          <Image
+            src={currantImage}
+            alt="productImage"
+            width={400}
+            height={400}
+            className="object-contain w-full h-full "
+          />
+        ) : (
+          <p className="text-gray-500 text-sm">No image found</p>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default ProductDetails;
