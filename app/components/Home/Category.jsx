bef2ea107cff86e68553fd2d8dac4cf075eebec0
Robi Mankhin <mankhinrobi@gmail.com>
2025-11-17 11:07:09 +0600

    home category options

--- app/components/Home/Category.jsx
+++ app/components/Home/Category.jsx
@@ -27,13 +27,19 @@ const Category = () => {
           <h3 className="font-inter font-medium text-[24px]">
             Browse By Category
           </h3>
-          <div className="flex items-center gap-8">
-            <div className="rounded-[15px] bg-[#EDEDED] w-40 h-32 flex items-center justify-center ">
-              <CiMobile4 className="text-[48px]" />
-            </div>
-            <div className="rounded-[15px] bg-[#EDEDED] w-40 h-32 flex items-center justify-center ">
+          <div className="flex items-center gap-6 flex-wrap">
+            {categories.map((item) => (
+              <div
+                key={item.name}
+                className="rounded-[15px] bg-[#EDEDED]  px-6 py-5 flex items-center justify-center "
+              >
+                {/* <CiMobile4 className="text-[48px]" /> */}
+                <p className="text-xl">{item.name}</p>
+              </div>
+            ))}
+            {/* <div className="rounded-[15px] bg-[#EDEDED] w-40 h-32 flex items-center justify-center ">
               <FaLaptopCode className="text-[48px]" />
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
