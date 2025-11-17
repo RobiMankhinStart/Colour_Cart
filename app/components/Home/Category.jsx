43540f560d4302e9732753beec45817fafdcaf76
Robi Mankhin <mankhinrobi@gmail.com>
2025-11-17 10:32:40 +0600

    removed function done

--- app/components/Home/Category.jsx
+++ app/components/Home/Category.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { CiMobile4 } from "react-icons/ci";
 import { FaLaptopCode } from "react-icons/fa";
 
 const Category = () => {
+  const [categories, setcategories] = useState([]);
+  useEffect(() => {
+    async function getcategories() {
+      try {
+        const res = await fetch("https://dummyjson.com/products/categories");
+        const data = await res.json();
+        const filteringIDs = data.products.filter((item) =>
+          localIDs.includes(item.id)
+        );
+        setcategories(filteringIDs);
+      } catch (error) {
+        console.log("fetching error : ", error);
+      }
+    }
+    getcategories();
+  }, []);
+  console.log("cartProducts", cartProducts);
   return (
     <div className="py-20">
       <div className="container">
