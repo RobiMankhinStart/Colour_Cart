6d08a41f65655ed9ebef2345b6c12b2746e716d7
Robi Mankhin <mankhinrobi@gmail.com>
2025-11-19 10:52:16 +0600

    fixing errors

--- app/components/Home/Sale.jsx
+++ app/components/Home/Sale.jsx
@@ -5,8 +5,8 @@ import banner2 from "../../../public/Banner2.png";
 const Sale = () => {
   return (
     <div>
-      <div className="w-full relative border h-[300px] sm:h-[400px] md:h-[500px]">
-        <Image src={banner2} style={{ objectFit: "cover" }} alt="banner" />
+      <div className=" h-[300px] sm:h-[400px] md:h-[500px]">
+        <Image src={banner2} alt="banner" className="w-full" />
       </div>
     </div>
   );
